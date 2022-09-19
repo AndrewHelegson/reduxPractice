68f6ce5052872b5904a68be613995bff5ab85ffa
AndrewHelegson <a.khrustikov@gmail.com>
2022-09-20 00:16:33 +0300

    just a random redux and git practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+  const dispatch = useDispatch()
+  const passengers = useSelector(state => state.passengers)
+  const rabbits = useSelector(state => state.rabbits)
+
+  const plusPassenger = () => {
+    dispatch({ type: "ADD_PASSENGERS", payload: 1 })
+  }
+  const minusPassenger = () => {
+    dispatch({ type: "REMOVE_PASSENGERS", payload: 1 })
+  }
+  const plusRabbit = () => {
+    dispatch({ type: "ADD_RABBITS", payload: 1 })
+  }
+  const minusRabbit = () => {
+    dispatch({ type: "REMOVE_RABBITS", payload: 1 })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="header" >ELEKTRICHKA PASSANGERS COUNTER</p>
+      <img className="pic" alt="34" src="https://s.ura.news/760/images/news/upload/news/393/070/1052393070/037417e5474c85587d1eadce47759a89_250x0_960.720.0.0.jpg" />
+      <p className="text">Количество пассажиров: <span className="number">{passengers}</span></p>
+      <div>
+        <button onClick={() => plusPassenger()} className="buttonPlus">+1 пассажир</button>
+        <button onClick={() => minusPassenger()} className="buttonMinus" >-1 пассажир</button>
+      </div>
+      <p className="text">Количество зайцев: <span className="number">{rabbits}</span></p>
+      <div>
+        <button onClick={() => plusRabbit()} className="buttonPlus">+1 безбилетник</button>
+        <button onClick={() => minusRabbit()} className="buttonMinus" >-1 безбилетник</button>
+      </div>
+      <div>
+        <p>Статистика:</p>
+        <p>процент безбилетников равен:</p>
+        <p>{Math.round((rabbits / passengers) * 100)}%</p>
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+
+const defaultStore = {
+  passengers: 0,
+  rabbits: 0
+}
+
+
+const reducer = (state = defaultStore, action) => {
+  switch (action.type) {
+    case "ADD_PASSENGERS":
+      return { ...state, passengers: state.passengers + action.payload }
+    case "REMOVE_PASSENGERS":
+      return { ...state, passengers: state.passengers - action.payload }
+    case "ADD_RABBITS":
+      return { ...state, rabbits: state.rabbits + action.payload }
+    case "REMOVE_RABBITS":
+      return { ...state, rabbits: state.rabbits - action.payload }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
